758868158e2f169f61f647a8e6bedb73bcbe03f3
Arthur Salles <a166003@dac.unicamp.br>
2023-05-06 18:26:02 -0300

    adding first codes

---
+++ build/CMakeFiles/chest_day.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chest_day.dir/src/chest_day.c.obj"
+  "CMakeFiles/chest_day.dir/src/chest_day.c.obj.d"
+  "chest_day.exe"
+  "chest_day.exe.manifest"
+  "chest_day.pdb"
+  "libchest_day.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chest_day.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/chest_day.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for chest_day.
+# This may be replaced when dependencies are built.
